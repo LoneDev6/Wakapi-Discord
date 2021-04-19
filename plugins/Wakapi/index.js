88f3cf5c384f08dc94675f9618064a3bd5599305
LoneDev6 <leoxy98@gmail.com>
2021-04-19 19:27:46 +0200

    Stuff
    - added new files/folders to .gitignore
    - removed the useless guild id configuration
    - changed the channel title matching logic to match ID.
    - changed config channel names to channel IDs

--- plugins/Wakapi/index.js
+++ plugins/Wakapi/index.js
@@ -1,143 +1,163 @@
 module.exports = (Plugin, Library) => {
-  const { Logger } = Library;
+    const {Logger} = Library;
 
-  return class Wakapi extends Plugin {
-    onStart() {
-      if (this.settings.enabled === false) return;
-      if (this.settings.wakapi.apikey === "")
-        return window.BdApi.alert(
-          "No Waka API key found! Place your api key in the settings menu of this plugin in order for the plugin to work."
-        );
-      Logger.log("Started");
-      let focussed = true;
-      this.focus = () => {
-        this.trySendHeartbeat();
-        focussed = true;
-      };
-      this.unfocus = () => {
-        focussed = false;
-      };
-      window.addEventListener("focus", this.focus);
-      window.addEventListener("blur", this.unfocus);
-
-      let trySendHeartbeat_hack = () => this.trySendHeartbeat();
-
-      window.setInterval(function () {
-        if (focussed) trySendHeartbeat_hack();
-      }, this.settings.wakapi.samplerate * 1000);
-    }
-
-    onStop() {
-      Logger.log("Stopped");
-      window.removeEventListener("focus", this.focus);
-      window.removeEventListener("blur", this.unfocus);
-    }
-
-    onMessage() {
-      this.trySendHeartbeat();
-    }
-
-    onSwitch() {
-      this.trySendHeartbeat();
-    }
-
-    trySendHeartbeat() {
-      if (this.settings.enabled !== true) return;
-      if (this.settings.wakapi.apikey === "") return;
-      // if the user supplied no guild and no categoy names we can assume they dont want to track anything
-      if (
-        this.settings.discord.trackingguilds === "" ||
-        this.settings.discord.categories === ""
-      )
-        return;
-      // dm channel
-      if (
-        ZeresPluginLibrary.DiscordAPI.currentGuild === null ||
-        ZeresPluginLibrary.DiscordAPI.currentGuild === undefined
-      )
-        return;
-      let project = "Tickets";
-      const category = ZeresPluginLibrary.DiscordAPI.currentChannel.category;
-      const guild_id = ZeresPluginLibrary.DiscordAPI.currentGuild.id;
-      const allowed_categories = this.settings.discord.categories;
-
-      if (category === null || category === undefined) return;
-      // guild check
-      if (!this.settings.discord.trackingguilds.includes(guild_id)) return;
-      // category check
-      if (!allowed_categories.includes(category.discordObject.name)) return;
-
-      // projectmapping
-      if (
-        this.settings.discord.projectmapping !== "" &&
-        this.settings.discord.projectmapping.includes(guild_id)
-      ) {
-        let projects = this.settings.discord.projectmapping.includes(",")
-          ? this.settings.discord.projectmapping.split(",")
-          : [this.settings.discord.projectmapping];
-
-        projects.forEach((p) => {
-          let mapped = p.split("=>");
-          if (mapped[0].trim() === guild_id) project = mapped[1].trim();
-        });
-      }
-
-      const request = require("request");
-      const os = require("os");
-      const { Buffer } = require("buffer");
-
-      const machine = os.hostname();
-      const system = os.type().replace("_NT", "");
-      const time = Math.round(Date.now() / 1000);
-
-      const heartbeat_obj = [
+    return class Wakapi extends Plugin
+    {
+        onStart()
         {
-          entity: "Tickets",
-          type: "Tickets",
-          category: "Tickets",
-          language: "Tickets",
-          editor: "Discord",
-          is_write: true,
-          machine: machine,
-          operating_system: system,
-          project,
-          time,
-        },
-      ];
+            if (this.settings.enabled === false)
+                return;
+            if (this.settings.wakapi.apikey === "")
+                return window.BdApi.alert(
+                    "No Waka API key found! Place your api key in the settings menu of this plugin in order for the plugin to work."
+                );
+            Logger.log("Started");
+            let focussed = true;
+            this.focus = () => {
+                this.trySendHeartbeat();
+                focussed = true;
+            };
+            this.unfocus = () => {
+                focussed = false;
+            };
+            window.addEventListener("focus", this.focus);
+            window.addEventListener("blur", this.unfocus);
 
-      const headersOpt = {
-        "content-type": "application/json",
-        "X-Machine-Name": machine,
-        "User-Agent": `wakatime/1.0.0 (${system}-idk) Discord/1.0.0 Discord-wakatime/1.0.0`,
-        Authorization: `Basic ${Buffer.from(
-          this.settings.wakapi.apikey
-        ).toString("base64")}`,
-      };
+            let trySendHeartbeat_hack = () => this.trySendHeartbeat();
 
-      const options = {
-        uri: this.settings.wakapi.apiurl + this.settings.wakapi.apikey,
-        method: "POST",
-        headers: headersOpt,
-        json: heartbeat_obj,
-      };
-
-      request(options, (e, r, b) => {
-        if (!e && b && r.statusCode === 201) {
-          Logger.log("Sent ticket activity");
-        } else {
-          window.BdApi.alert(
-            "Error",
-            "Wakapi error: " + b.toString() + " | " + e.toString()
-          );
-          Logger.err(e);
-          Logger.err(b);
+            window.setInterval(function () {
+                if (focussed)
+                    trySendHeartbeat_hack();
+            }, this.settings.wakapi.samplerate * 1000);
         }
-      });
-    }
 
-    getSettingsPanel() {
-      const panel = this.buildSettingsPanel();
-      return panel.getElement();
-    }
-  };
+        onStop()
+        {
+            Logger.log("Stopped");
+            window.removeEventListener("focus", this.focus);
+            window.removeEventListener("blur", this.unfocus);
+        }
+
+        onMessage()
+        {
+            this.trySendHeartbeat();
+        }
+
+        onSwitch()
+        {
+            this.trySendHeartbeat();
+        }
+
+        trySendHeartbeat()
+        {
+            if (this.settings.enabled !== true)
+                return;
+            if (this.settings.wakapi.apikey === "")
+                return;
+            // if the user supplied no categoy names we can assume they dont want to track anything
+            if (
+                this.settings.discord.trackingguilds === "" ||
+                this.settings.discord.categories === ""
+            )
+                return;
+
+            // dm channel
+            if (
+                ZeresPluginLibrary.DiscordAPI.currentGuild === null ||
+                ZeresPluginLibrary.DiscordAPI.currentGuild === undefined
+            )
+                return;
+
+            let project = "Tickets";
+            const category = ZeresPluginLibrary.DiscordAPI.currentChannel.category;
+            const allowed_categories = this.settings.discord.categories;
+
+            if (category === null || category === undefined)
+                return;
+
+            const category_id = category.discordObject.id;
+
+            // category check
+            if (!allowed_categories.includes(category_id))
+                return;
+
+            // projectmapping
+            if (
+                this.settings.discord.projectmapping !== "" &&
+                this.settings.discord.projectmapping.includes(category_id)
+            )
+            {
+                let projects = this.settings.discord.projectmapping.includes(",")
+                    ? this.settings.discord.projectmapping.split(",")
+                    : [this.settings.discord.projectmapping];
+
+                projects.forEach((p) => {
+                    let mapped = p.split("=>");
+                    if (mapped[0].trim() === category_id)
+                        project = mapped[1].trim();
+                });
+            }
+
+            const request = require("request");
+            const os = require("os");
+            const {Buffer} = require("buffer");
+
+            const machine = os.hostname();
+            const system = os.type().replace("_NT", "");
+            const time = Math.round(Date.now() / 1000);
+
+            const heartbeat_obj = [
+                {
+                    entity: "Tickets",
+                    type: "Tickets",
+                    category: "Tickets",
+                    language: "Tickets",
+                    editor: "Discord",
+                    is_write: true,
+                    machine: machine,
+                    operating_system: system,
+                    project,
+                    time,
+                },
+            ];
+
+            const headersOpt = {
+                "content-type": "application/json",
+                "X-Machine-Name": machine,
+                "User-Agent": `wakatime/1.0.0 (${system}-idk) Discord/1.0.0 Discord-wakatime/1.0.0`,
+                Authorization: `Basic ${Buffer.from(
+                    this.settings.wakapi.apikey
+                ).toString("base64")}`,
+            };
+
+            const options = {
+                uri: this.settings.wakapi.apiurl + this.settings.wakapi.apikey,
+                method: "POST",
+                headers: headersOpt,
+                json: heartbeat_obj,
+            };
+
+            request(options, (e, r, b) => {
+                if (!e && b && r.statusCode === 201)
+                {
+                    Logger.log("Sent ticket activity");
+                }
+                else
+                {
+                    window.BdApi.alert(
+                        "Error",
+                        "Wakapi error: " + b.toString() + " | " + e.toString()
+                    );
+                    Logger.err(e);
+                    Logger.err(b);
+                }
+            });
+        }
+
+        getSettingsPanel()
+        {
+            const panel = this.buildSettingsPanel();
+            return panel.getElement();
+        }
+    };
 };
--- release/Wakapi.plugin.js
+++ release/Wakapi.plugin.js
@@ -32,7 +32,7 @@
 @else@*/
 
 module.exports = (() => {
-    const config = {"main":"index.js","info":{"name":"Wakapi","authors":[{"name":"LoneDev","discord_id":"289137568144949248","github_username":"LoneDev6","twitter_username":"LoneDev6"},{"name":"MeerBiene","discord_id":"686669011601326281","github_username":"MeerBiene"}],"version":"0.0.2","invite":"n4WfXaWNQc","website":"https://github.com/LoneDev6/Wakapi-Discord#readme","description":"Enables Wakatime tracking of time spent in support channels.","updateUrl":"https://raw.githubusercontent.com/LoneDev6/Wakapi-Discord/main/release/Wakapi.plugin.js","github":"https://github.com/LoneDev6/Wakapi-Discord#readme","github_raw":"https://raw.githubusercontent.com/LoneDev6/Wakapi-Discord/main/release/Wakapi.plugin.js"},"changelog":[{"title":"Sample Rate","items":["add smaple rate setting for less network usage"]},{"title":"Settings","items":["added general settings","added discord settings to enable what channel names on which guilds are tracked"]},{"title":"Tracking on other guilds","type":"fixed","items":["Fixed bug where the plugin tracked time spent in support channels on other guilds"]}],"defaultConfig":[{"type":"switch","id":"enabled","name":"Tracking Enabled","value":true},{"type":"category","id":"wakapi","name":"Wakapi Settings","collapsible":true,"shown":false,"settings":[{"type":"textbox","id":"apikey","name":"Wakapi API Key","note":"API Key needed to interact with Wakapi","value":""},{"type":"textbox","id":"apiurl","name":"Wakapi Url","note":"API Url where your Wakapi instance lives","value":"https://wakapi.dev/api/heartbeat"},{"type":"slider","id":"samplerate","name":"Sample Rate","note":"Send a heartbeat every ... seconds","value":90,"min":30,"max":240,"markers":[30,60,90,120,150,180,210,240],"stickToMarkers":true}]},{"type":"category","id":"discord","name":"Discord Settings","collapsible":true,"shown":false,"settings":[{"type":"textbox","id":"trackingguilds","name":"Tracking Guilds","note":"The guild(s) in which you want to track your time. IDs! For multiple guilds, just seperate the guild IDs by comma. If left empty the plugin tracks the time on all guilds.","placeholder":"1234567891011, 1110987654321","value":""},{"type":"textbox","id":"categories","name":"Category Names","note":"Put the category names that indicate a support category and trigger the time tracking action here. For more than one just seperate them by comma.","value":"tickets, support"},{"type":"textbox","id":"projectmapping","name":"Project Mapping","note":"If you want to map the tracked guilds to a certain project, put them in here like so: {GUILID} => {PROJECTNAME}. If left empty, the project shown in wakapi will be \"tickets\".","placeholder":"1234567891011 => ExampleProject, 1110987654321 => ProjectExample","value":""}]}]};
+    const config = {"main":"index.js","info":{"name":"Wakapi","authors":[{"name":"LoneDev","discord_id":"289137568144949248","github_username":"LoneDev6","twitter_username":"LoneDev6"},{"name":"MeerBiene","discord_id":"686669011601326281","github_username":"MeerBiene"}],"version":"0.0.2","invite":"n4WfXaWNQc","website":"https://github.com/LoneDev6/Wakapi-Discord#readme","description":"Enables Wakatime tracking of time spent in support channels.","updateUrl":"https://raw.githubusercontent.com/LoneDev6/Wakapi-Discord/main/release/Wakapi.plugin.js","github":"https://github.com/LoneDev6/Wakapi-Discord#readme","github_raw":"https://raw.githubusercontent.com/LoneDev6/Wakapi-Discord/main/release/Wakapi.plugin.js"},"changelog":[{"title":"Sample Rate","items":["add smaple rate setting for less network usage"]},{"title":"Settings","items":["added general settings","added discord settings to enable what channel names on which guilds are tracked"]},{"title":"Tracking on other guilds","type":"fixed","items":["Fixed bug where the plugin tracked time spent in support channels on other guilds"]}],"defaultConfig":[{"type":"switch","id":"enabled","name":"Tracking Enabled","value":true},{"type":"category","id":"wakapi","name":"Wakapi Settings","collapsible":true,"shown":true,"settings":[{"type":"textbox","id":"apikey","name":"Wakapi API Key","note":"API Key needed to interact with Wakapi","value":""},{"type":"textbox","id":"apiurl","name":"Wakapi Url","note":"API Url where your Wakapi instance lives","value":"https://wakapi.dev/api/heartbeat"},{"type":"slider","id":"samplerate","name":"Sample Rate","note":"Send a heartbeat every ... seconds","value":90,"min":30,"max":240,"markers":[30,60,90,120,150,180,210,240],"stickToMarkers":true}]},{"type":"category","id":"discord","name":"Discord Settings","collapsible":true,"shown":true,"settings":[{"type":"textbox","id":"categories","name":"Category ID","note":"Put the category ID that indicate a support category and trigger the time tracking action here. For more than one just separate them by comma.","placeholder":"1234567891011, 1110987654321","value":""},{"type":"textbox","id":"projectmapping","name":"Project Mapping","note":"If you want to map the tracked categories to a certain project, put them in here like so: {CATEGORYID} => {PROJECTNAME}. If left empty, the project shown in wakapi will be \"Tickets\".","placeholder":"1234567891011 => ExampleProject, 1110987654321 => ProjectExample","value":""}]}]};
 
     return !global.ZeresPluginLibrary ? class {
         constructor() {this._config = config;}
@@ -56,147 +56,167 @@ module.exports = (() => {
         stop() {}
     } : (([Plugin, Api]) => {
         const plugin = (Plugin, Library) => {
-  const { Logger } = Library;
+    const {Logger} = Library;
 
-  return class Wakapi extends Plugin {
-    onStart() {
-      if (this.settings.enabled === false) return;
-      if (this.settings.wakapi.apikey === "")
-        return window.BdApi.alert(
-          "No Waka API key found! Place your api key in the settings menu of this plugin in order for the plugin to work."
-        );
-      Logger.log("Started");
-      let focussed = true;
-      this.focus = () => {
-        this.trySendHeartbeat();
-        focussed = true;
-      };
-      this.unfocus = () => {
-        focussed = false;
-      };
-      window.addEventListener("focus", this.focus);
-      window.addEventListener("blur", this.unfocus);
-
-      let trySendHeartbeat_hack = () => this.trySendHeartbeat();
-
-      window.setInterval(function () {
-        if (focussed) trySendHeartbeat_hack();
-      }, this.settings.wakapi.samplerate * 1000);
-    }
-
-    onStop() {
-      Logger.log("Stopped");
-      window.removeEventListener("focus", this.focus);
-      window.removeEventListener("blur", this.unfocus);
-    }
-
-    onMessage() {
-      this.trySendHeartbeat();
-    }
-
-    onSwitch() {
-      this.trySendHeartbeat();
-    }
-
-    trySendHeartbeat() {
-      if (this.settings.enabled !== true) return;
-      if (this.settings.wakapi.apikey === "") return;
-      // if the user supplied no guild and no categoy names we can assume they dont want to track anything
-      if (
-        this.settings.discord.trackingguilds === "" ||
-        this.settings.discord.categories === ""
-      )
-        return;
-      // dm channel
-      if (
-        ZeresPluginLibrary.DiscordAPI.currentGuild === null ||
-        ZeresPluginLibrary.DiscordAPI.currentGuild === undefined
-      )
-        return;
-      let project = "Tickets";
-      const category = ZeresPluginLibrary.DiscordAPI.currentChannel.category;
-      const guild_id = ZeresPluginLibrary.DiscordAPI.currentGuild.id;
-      const allowed_categories = this.settings.discord.categories;
-
-      if (category === null || category === undefined) return;
-      // guild check
-      if (!this.settings.discord.trackingguilds.includes(guild_id)) return;
-      // category check
-      if (!allowed_categories.includes(category.discordObject.name)) return;
-
-      // projectmapping
-      if (
-        this.settings.discord.projectmapping !== "" &&
-        this.settings.discord.projectmapping.includes(guild_id)
-      ) {
-        let projects = this.settings.discord.projectmapping.includes(",")
-          ? this.settings.discord.projectmapping.split(",")
-          : [this.settings.discord.projectmapping];
-
-        projects.forEach((p) => {
-          let mapped = p.split("=>");
-          if (mapped[0].trim() === guild_id) project = mapped[1].trim();
-        });
-      }
-
-      const request = require("request");
-      const os = require("os");
-      const { Buffer } = require("buffer");
-
-      const machine = os.hostname();
-      const system = os.type().replace("_NT", "");
-      const time = Math.round(Date.now() / 1000);
-
-      const heartbeat_obj = [
+    return class Wakapi extends Plugin
+    {
+        onStart()
         {
-          entity: "Tickets",
-          type: "Tickets",
-          category: "Tickets",
-          language: "Tickets",
-          editor: "Discord",
-          is_write: true,
-          machine: machine,
-          operating_system: system,
-          project,
-          time,
-        },
-      ];
+            if (this.settings.enabled === false)
+                return;
+            if (this.settings.wakapi.apikey === "")
+                return window.BdApi.alert(
+                    "No Waka API key found! Place your api key in the settings menu of this plugin in order for the plugin to work."
+                );
+            Logger.log("Started");
+            let focussed = true;
+            this.focus = () => {
+                this.trySendHeartbeat();
+                focussed = true;
+            };
+            this.unfocus = () => {
+                focussed = false;
+            };
+            window.addEventListener("focus", this.focus);
+            window.addEventListener("blur", this.unfocus);
 
-      const headersOpt = {
-        "content-type": "application/json",
-        "X-Machine-Name": machine,
-        "User-Agent": `wakatime/1.0.0 (${system}-idk) Discord/1.0.0 Discord-wakatime/1.0.0`,
-        Authorization: `Basic ${Buffer.from(
-          this.settings.wakapi.apikey
-        ).toString("base64")}`,
-      };
+            let trySendHeartbeat_hack = () => this.trySendHeartbeat();
 
-      const options = {
-        uri: this.settings.wakapi.apiurl + this.settings.wakapi.apikey,
-        method: "POST",
-        headers: headersOpt,
-        json: heartbeat_obj,
-      };
-
-      request(options, (e, r, b) => {
-        if (!e && b && r.statusCode === 201) {
-          Logger.log("Sent ticket activity");
-        } else {
-          window.BdApi.alert(
-            "Error",
-            "Wakapi error: " + b.toString() + " | " + e.toString()
-          );
-          Logger.err(e);
-          Logger.err(b);
+            window.setInterval(function () {
+                if (focussed)
+                    trySendHeartbeat_hack();
+            }, this.settings.wakapi.samplerate * 1000);
         }
-      });
-    }
 
-    getSettingsPanel() {
-      const panel = this.buildSettingsPanel();
-      return panel.getElement();
-    }
-  };
+        onStop()
+        {
+            Logger.log("Stopped");
+            window.removeEventListener("focus", this.focus);
+            window.removeEventListener("blur", this.unfocus);
+        }
+
+        onMessage()
+        {
+            this.trySendHeartbeat();
+        }
+
+        onSwitch()
+        {
+            this.trySendHeartbeat();
+        }
+
+        trySendHeartbeat()
+        {
+            if (this.settings.enabled !== true)
+                return;
+            if (this.settings.wakapi.apikey === "")
+                return;
+            // if the user supplied no categoy names we can assume they dont want to track anything
+            if (
+                this.settings.discord.trackingguilds === "" ||
+                this.settings.discord.categories === ""
+            )
+                return;
+
+            // dm channel
+            if (
+                ZeresPluginLibrary.DiscordAPI.currentGuild === null ||
+                ZeresPluginLibrary.DiscordAPI.currentGuild === undefined
+            )
+                return;
+
+            let project = "Tickets";
+            const category = ZeresPluginLibrary.DiscordAPI.currentChannel.category;
+            const allowed_categories = this.settings.discord.categories;
+
+            if (category === null || category === undefined)
+                return;
+
+            const category_id = category.discordObject.id;
+
+            // category check
+            if (!allowed_categories.includes(category_id))
+                return;
+
+            // projectmapping
+            if (
+                this.settings.discord.projectmapping !== "" &&
+                this.settings.discord.projectmapping.includes(category_id)
+            )
+            {
+                let projects = this.settings.discord.projectmapping.includes(",")
+                    ? this.settings.discord.projectmapping.split(",")
+                    : [this.settings.discord.projectmapping];
+
+                projects.forEach((p) => {
+                    let mapped = p.split("=>");
+                    if (mapped[0].trim() === category_id)
+                        project = mapped[1].trim();
+                });
+            }
+
+            const request = require("request");
+            const os = require("os");
+            const {Buffer} = require("buffer");
+
+            const machine = os.hostname();
+            const system = os.type().replace("_NT", "");
+            const time = Math.round(Date.now() / 1000);
+
+            const heartbeat_obj = [
+                {
+                    entity: "Tickets",
+                    type: "Tickets",
+                    category: "Tickets",
+                    language: "Tickets",
+                    editor: "Discord",
+                    is_write: true,
+                    machine: machine,
+                    operating_system: system,
+                    project,
+                    time,
+                },
+            ];
+
+            const headersOpt = {
+                "content-type": "application/json",
+                "X-Machine-Name": machine,
+                "User-Agent": `wakatime/1.0.0 (${system}-idk) Discord/1.0.0 Discord-wakatime/1.0.0`,
+                Authorization: `Basic ${Buffer.from(
+                    this.settings.wakapi.apikey
+                ).toString("base64")}`,
+            };
+
+            const options = {
+                uri: this.settings.wakapi.apiurl + this.settings.wakapi.apikey,
+                method: "POST",
+                headers: headersOpt,
+                json: heartbeat_obj,
+            };
+
+            request(options, (e, r, b) => {
+                if (!e && b && r.statusCode === 201)
+                {
+                    Logger.log("Sent ticket activity");
+                }
+                else
+                {
+                    window.BdApi.alert(
+                        "Error",
+                        "Wakapi error: " + b.toString() + " | " + e.toString()
+                    );
+                    Logger.err(e);
+                    Logger.err(b);
+                }
+            });
+        }
+
+        getSettingsPanel()
+        {
+            const panel = this.buildSettingsPanel();
+            return panel.getElement();
+        }
+    };
 };
         return plugin(Plugin, Api);
     })(global.ZeresPluginLibrary.buildPlugin(config));
